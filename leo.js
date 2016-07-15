8975374dd34ba56c4a5f5bebe5d9f28764441742
Leon0238 <lj.menezes2@gmail.com>
2016-07-14 21:25:05 -0300

    Add files via upload

---
+++ leo.js
@@ -0,0 +1,39 @@
+﻿$(document).ready(function () {
+    $(".port").click(function () {
+        $(".portDrop").slideToggle();
+    });
+
+    $(".about, .saibaMais").click(function () {
+        $(".aboutMe").fadeIn();
+        $(".ResumeScreen, .ContactScreen").fadeOut(200);
+        $("body").css("background-image", "url('images/aboutBg.png')")
+    });
+
+    $(".back, .home").click(function () {
+        $(".aboutMe, .ResumeScreen, .ContactScreen").fadeOut(200);
+        $("body").css("background-image", "url('images/city.jpg')")
+    });
+
+    $(".resume").click(function () {
+        $(".ResumeScreen").fadeIn();
+        $(".aboutMe, .ContactScreen").fadeOut(200);
+        $("body").css("background-image", "url('images/bgResume.jpg')")
+    });
+
+    $(".contact").click(function () {
+        $(".ContactScreen").fadeIn();
+        $(".aboutMe, .ResumeScreen").fadeOut(200);
+        $("body").css("background-image", "url('images/bgContact.jpg')")
+    });
+
+    $('#send').click(function () {
+        if ($("#name").val() == "" || $("#email").val() == "" || $("#msg").val() == "") {            
+            $("#send").prop('disabled', true);
+            $('#name, #email, #msg').addClass('required');       
+        }
+    });
+
+    $("#name, #email, #msg").keyup(function () {
+        $("#send").prop('disabled', false);
+    });
+});
